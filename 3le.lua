cbbbe3fcdc892b39048cf676159b90df696d3a52
ewewe514 <eeeiqjjj221@gmail.com>
2025-05-08 18:41:47 -0400

    Create 3le.lua

---
+++ 3le.lua
@@ -0,0 +1,22 @@
+task.spawn(function()
+    while true do
+        local activateRemote = game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Network"):WaitForChild("RemotePromise"):WaitForChild("Remotes"):FindFirstChild("C_ActivateObject")
+
+        if activateRemote then
+            local runtimeItems = game.Workspace:WaitForChild("RuntimeItems")
+
+            for _, ammoContainer in pairs(runtimeItems:GetChildren()) do
+                if ammoContainer:IsA("Model") and ammoContainer.Name == "ShotgunShells" then
+                    local ammo = ammoContainer:FindFirstChild("ShotgunShells")
+                    if ammo then
+                        local targetPart = ammo:FindFirstChildWhichIsA("BasePart") or ammo
+
+                        local args = { targetPart.Parent }
+                        activateRemote:FireServer(unpack(args))
+                    end
+                end
+            end
+        end
+        task.wait(0.3) -- Keeping your delay between activations
+    end
+end)
